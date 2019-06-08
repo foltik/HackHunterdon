164a8bec7d1131769bd566261ea561d9ab28c07a
Tyler Chen <tylerc6@illinois.edu>
2019-06-08 15:34:41 -0400

    changed to yarn because that solved an issue, added some sample tab navigation

--- hh-19-app/App.tsx
+++ hh-19-app/App.tsx
@@ -1,19 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator, createAppContainer} from 'react-navigation';
+import AntDesign from "react-native-vector-icons/AntDesign"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>This is the home screen</Text>
+      </View>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+class ProfileScreen extends Component {
+  render () {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>My name is Jacques I am a gigachad</Text>
+      </View>
+    );
+  }
+}
+
+const MainNavigator = createBottomTabNavigator(
+  {
+    Home: {screen: HomeScreen},
+    Profile: {screen: ProfileScreen},
   },
-});
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = AntDesign;
+        let iconName: string;
+        if (routeName === 'Home') {
+          iconName = 'home';
+        } 
+        else if (routeName === 'Profile') {
+          iconName = `user`;
+        }
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      }
+    })
+  }
+);
+
+const App = createAppContainer(MainNavigator);
+
+export default App;
+
