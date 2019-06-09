6f0f312c665f7a2ce1c82597ef679e5e0fd8376f
Tyler Chen <tylerc6@illinois.edu>
2019-06-09 06:32:44 -0400

    added safeview

--- hh-19-app/components/HomeScreen.tsx
+++ hh-19-app/components/HomeScreen.tsx
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { fb, uid } from '../App'
+import { SafeAreaView } from 'react-navigation';
+import { Card } from 'react-native-elements';
 
 export class HomeScreen extends Component {
+  state = {
+    data: []
+  }
+
+  componentDidMount() {
+    let eatenRef = fb.firestore().collection("users").doc(uid).collection("consumed");
+    eatenRef.get().then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        this.setState({ data: [...this.state.data, doc.data()] });
+      });
+    })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
+    console.log(this.state.data);
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>This is the home screen</Text>
-      </View>
+      <SafeAreaView style={{ flex: 1, justifyContent: "center"}}>
+        <Text style={{fontSize: 20}}>Here's what you've eaten today:</Text>
+        <FlatList
+          data={this.state.data}
+          renderItem={({ item }) => <Card><Text>{item.name}</Text></Card>}
+        />
+      </SafeAreaView>
     );
   }
 }
